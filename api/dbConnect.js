ec785ca84116b4dab989c0199b4466cc4a9e3420
Ilyaçe Regaibi <ilyace.regaibi@gmail.com>
2020-02-03 17:28:51 +0100

    Create dbConnect file for db connection

---
+++ api/dbConnect.js
@@ -0,0 +1,8 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize("clash_of_worlds", "root", "root", {
+  host: "database",
+  dialect: "postgres"
+});
+
+module.exports = sequelize;
