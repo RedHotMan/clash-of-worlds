52b7043bdc544c2830adfcfd8c6206cf2a794723
Ilyaçe Regaibi <ilyace.regaibi@gmail.com>
2020-02-03 17:30:11 +0100

    Launch the apollo server only when the db is connected

--- api/index.js
+++ api/index.js
@@ -1,13 +1,24 @@
 const { ApolloServer } = require('apollo-server');
-require('./models');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
+const sequelize = require('./dbConnect');
 
 const server = new ApolloServer({
   typeDefs,
   resolvers
 });
 
-server.listen().then(({url}) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection to the database has been established successfully.");
+    server.listen({ port: 4000 }).then(({ url }) => {
+      console.log(`🚀  Server ready at ${url}`);
+    });
+  })
+  .catch(err => {
+    console.error("Unable to connect to the database:", err);
+  });
+
+
+  //TODO: Write a script to feed db with some mocked data
