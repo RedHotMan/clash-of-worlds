6075122bd9682d7723503640b0d6b4e8cb983271
Ilyaçe Regaibi <ilyace.regaibi@gmail.com>
2020-02-03 17:33:46 +0100

    models/index file will now only export and objection containing models

--- api/models/index.js
+++ api/models/index.js
@@ -1,36 +1,7 @@
-const Sequelize = require("sequelize");
+const sequelize = require("../dbConnect");
 
-const sequelize = new Sequelize('clash_of_worlds', 'root', 'root', {
-  host: 'database',
-  dialect: 'postgres'
-});
-
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log("Connection has been established successfully.");
-  })
-  .catch(err => {
-    console.error("Unable to connect to the database:", err);
-});
-
-exports.models = {
+const models = {
   User: sequelize.import('./user'),
 };
 
-const forceDataSync = false
-
-const initDb = async () => {
-  await models.User.create({
-    firstname: "admin",
-    lastname: "admin",
-    email: "test@email.com",
-    password: "psswrd",
-  });
-}
-
-sequelize.sync({ force: forceDataSync }).then(() => {
-  if (forceDataSync) {
-    initDb();
-  }
-});
+module.exports = models;
