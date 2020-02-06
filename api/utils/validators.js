b237f77259646e219ee1d2de8f95f9bac9f30110
Ilyaçe Regaibi <ilyace.regaibi@gmail.com>
2020-02-06 16:05:19 +0100

    Create registration data validator util

---
+++ api/utils/validators.js
@@ -0,0 +1,33 @@
+const validateEmail = email => {
+  const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return emailRegex.test(String(email).toLowerCase());
+};
+
+module.exports.registerValidation = (username, email, password) => {
+  const errors = {};
+
+  if (username.trim().length === 0) {
+    errors.username = "Username must not be empty";
+  } else if (username.trim().length < 4) {
+    errors.username = "Username must have at least 4 letters";
+  }
+
+  if (email.trim().length === 0) {
+    errors.email = "Email must not be empty";
+  } else if (!validateEmail(email)) {
+    errors.email = "Email must be a valid email address";
+  }
+
+  if (password.trim().length === 0) {
+    errors.password = "Password must not be empty";
+  } else if (password.trim().length < 4) {
+    errors.password = "Password must have at least 4 characters";
+  } else if (password.trim().includes(" ")) {
+    errors.password = "Password must not contain spaces";
+  }
+
+  return {
+    errors,
+    valid: Object.keys(errors).length < 1
+  };
+};
