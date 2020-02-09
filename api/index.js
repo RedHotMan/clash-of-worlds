a51b150f92cbe967c17d56d5818494345f598712
Ilyaçe Regaibi <ilyace.regaibi@gmail.com>
2020-02-09 10:39:28 +0100

    Feed db with some data when node server is up

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 const { ApolloServer } = require("apollo-server");
 const sequelize = require("./dbConnect");
+const { User, Planet } = require("./models");
+const { ADMIN } = require("./utils/roles");
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
 
@@ -14,7 +16,42 @@ sequelize
     console.log(
       "Connection to the database has been established successfully."
     );
-    sequelize.sync();
+
+    sequelize.sync({ force: true }).then(() => {
+      Planet.create(
+        {
+          name: "DonutsFactory",
+          users: [
+            {
+              username: "Bejita",
+              email: "bejita@email.com",
+              password: "psswrd"
+            }
+          ]
+        },
+        {
+          include: [{ association: Planet.hasMany(User) }]
+        }
+      );
+
+      Planet.create({
+        name: "RaccoonsOfAsgard"
+      });
+      Planet.create({
+        name: "DucksInvaders"
+      });
+      Planet.create({
+        name: "SkizoCats"
+      });
+
+      User.create({
+        username: "admin",
+        email: "admin@admin.com",
+        password: "admin",
+        role: ADMIN,
+        planetId: 1
+      });
+    });
     server.listen({ port: 4000 }).then(({ url }) => {
       console.log(`🚀  Server ready at ${url}`);
     });
@@ -23,4 +60,4 @@ sequelize
     console.error("Unable to connect to the database:", err);
   });
 
-//TODO: Write a script to feed db with some mocked data
+//TODO: Write a BETTER script to feed db with some mocked data
