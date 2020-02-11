d615aa5cc0feea25e281652f12535b5fa6602822
Ilyaçe Regaibi <ilyace.regaibi@gmail.com>
2020-02-11 23:16:08 +0100

    Update db sync when server start

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const { ApolloServer } = require("apollo-server");
 const sequelize = require("./dbConnect");
 const { User, Planet } = require("./models");
-const { ADMIN } = require("./utils/roles");
+const roles = require("./utils/roles");
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
 
@@ -21,8 +21,29 @@ sequelize
       Planet.create(
         {
           name: "DonutsFactory",
+          leaderId: "13dc1e5c-eb36-4919-9f8f-4978019dd127",
           users: [
             {
+              id: "13dc1e5c-eb36-4919-9f8f-4978019dd127",
+              username: "admin",
+              email: "admin@admin.com",
+              password: "admin",
+              role: roles.ADMIN
+            }
+          ]
+        },
+        {
+          include: [{ association: Planet.hasMany(User) }]
+        }
+      );
+
+      Planet.create(
+        {
+          name: "DonutsFactory",
+          leaderId: "48bd03b3-0332-4ed0-b098-bc2da667081c",
+          users: [
+            {
+              id: "48bd03b3-0332-4ed0-b098-bc2da667081c",
               username: "Bejita",
               email: "bejita@email.com",
               password: "psswrd"
@@ -34,23 +55,41 @@ sequelize
         }
       );
 
-      Planet.create({
-        name: "RaccoonsOfAsgard"
-      });
-      Planet.create({
-        name: "DucksInvaders"
-      });
-      Planet.create({
-        name: "SkizoCats"
-      });
+      Planet.create(
+        {
+          name: "DucksInvaders",
+          leaderId: "26d16c57-1629-482b-a7d2-e8dab6097887",
+          users: [
+            {
+              id: "26d16c57-1629-482b-a7d2-e8dab6097887",
+              username: "Mugen",
+              email: "mugen@email.com",
+              password: "psswrd"
+            }
+          ]
+        },
+        {
+          include: [{ association: Planet.hasMany(User) }]
+        }
+      );
 
-      User.create({
-        username: "admin",
-        email: "admin@admin.com",
-        password: "admin",
-        role: ADMIN,
-        planetId: 1
-      });
+      Planet.create(
+        {
+          name: "SkizoCats",
+          leaderId: "f0010344-8011-4f10-82d3-342ba671fb6f",
+          users: [
+            {
+              id: "f0010344-8011-4f10-82d3-342ba671fb6f",
+              username: "Goku",
+              email: "goku@email.com",
+              password: "psswrd"
+            }
+          ]
+        },
+        {
+          include: [{ association: Planet.hasMany(User) }]
+        }
+      );
     });
     server.listen({ port: 4000 }).then(({ url }) => {
       console.log(`🚀  Server ready at ${url}`);
@@ -59,5 +98,3 @@ sequelize
   .catch(err => {
     console.error("Unable to connect to the database:", err);
   });
-
-//TODO: Write a BETTER script to feed db with some mocked data
