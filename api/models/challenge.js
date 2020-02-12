e8bba7f5c04fdeb6e0ec0c5e90bb2f8dab2ef4a3
Ilyaçe Regaibi <ilyace.regaibi@gmail.com>
2020-02-12 14:25:41 +0100

    Create challenge model

---
+++ api/models/challenge.js
@@ -0,0 +1,25 @@
+const { CHALLENGE_ADMIN_STATE } = require("../utils/constants");
+
+const challenge = (sequelize, DataTypes) => {
+  const Challenge = sequelize.define("challenge", {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    admin_state: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: CHALLENGE_ADMIN_STATE.WAITING
+    }
+  });
+
+  return Challenge;
+};
+
+module.exports = challenge;
--- api/models/index.js
+++ api/models/index.js
@@ -2,7 +2,8 @@ const sequelize = require("../dbConnect");
 
 const models = {
   User: sequelize.import("./user"),
-  Planet: sequelize.import("./planet")
+  Planet: sequelize.import("./planet"),
+  Challenge: sequelize.import("./challenge")
 };
 
 models.Planet.hasMany(models.User);
