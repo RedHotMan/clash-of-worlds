4ec40b39814fbc12a3aca139a08ae7d4cd9c32da
Ilyaçe Regaibi <ilyace.regaibi@gmail.com>
2020-03-06 17:42:28 +0100

    Allow to set a custom expiration time to a token

--- api/utils/generateToken.js
+++ api/utils/generateToken.js
@@ -1,12 +1,12 @@
 const config = require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
-const generateToken = async user => {
+const generateToken = async (user, expiresIn = "1h") => {
   return await jwt.sign(
     { id: user.id, username: user.username, email: user.email },
     process.env.SECRET_KEY,
     {
-      expiresIn: "1h"
+      expiresIn: expiresIn
     }
   );
 };
