4c4c91ac10b586bcb0c6a9619c87b17909bb7ca7
Ilyaçe Regaibi <ilyace.regaibi@gmail.com>
2020-03-09 10:25:39 +0100

    Make the token generated valid for a day

--- api/utils/generateToken.js
+++ api/utils/generateToken.js
@@ -1,7 +1,7 @@
 const config = require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
-const generateToken = async (user, expiresIn = "1h") => {
+const generateToken = async (user, expiresIn = "1d") => {
   return await jwt.sign(
     { id: user.id, username: user.username, email: user.email },
     process.env.SECRET_KEY,
