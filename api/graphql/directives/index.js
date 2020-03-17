e2ed6c4aaa055b4fc4f13c065c070675668baaa9
Ilyaçe Regaibi <ilyace.regaibi@gmail.com>
2020-03-17 15:10:35 +0100

    Create index file in directives folder for importation

---
+++ api/graphql/directives/index.js
@@ -0,0 +1,11 @@
+const StringConstraintDirective = require("../directives/StringConstraintDirective");
+const DateConstraintDirective = require("../directives/DateConstraintDirective");
+const NumberConstraintDirective = require("../directives/NumberConstraintDirective");
+const AuthDirective = require("../directives/AuthDirective");
+
+module.exports = {
+  StringConstraintDirective,
+  DateConstraintDirective,
+  NumberConstraintDirective,
+  AuthDirective
+};
