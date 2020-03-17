15c0083d9ba85bc383b1d892b9f2523a0517ebab
Ilyaçe Regaibi <ilyace.regaibi@gmail.com>
2020-03-17 15:13:31 +0100

    Use directives to validate data

--- api/graphql/typeDefs.js
+++ api/graphql/typeDefs.js
@@ -1,6 +1,36 @@
 const { gql } = require("apollo-server");
 
 const typeDefs = gql`
+  directive @string(
+    maxLength: Int
+    minLength: Int
+    type: StringType
+  ) on INPUT_FIELD_DEFINITION
+
+  directive @number(min: Int, max: Int) on INPUT_FIELD_DEFINITION
+
+  directive @role on INPUT_FIELD_DEFINITION
+
+  directive @date(
+    isAfter: DateType
+    isBefore: DateType
+    format: String
+  ) on INPUT_FIELD_DEFINITION
+
+  directive @auth on FIELD_DEFINITION
+
+  enum StringType {
+    EMAIL
+    PASSWORD
+    ROLE
+  }
+
+  enum DateType {
+    YESTERDAY
+    TODAY
+    TOMORROW
+  }
+
   type User {
     id: ID!
     username: String!
@@ -22,7 +52,7 @@ const typeDefs = gql`
   }
 
   type Challenge {
-    id: Int!
+    id: ID!
     state: String
     adminState: String!
     description: String!
@@ -34,41 +64,43 @@ const typeDefs = gql`
   }
 
   input RegisterInput {
-    username: String!
-    email: String!
-    password: String!
-    role: String
+    username: String! @string(minLength: 4)
+    email: String! @string(type: EMAIL)
+    password: String! @string(type: PASSWORD, minLength: 4)
+    role: String @string(type: ROLE)
     planetId: Int!
+  }
+
+  input CreateChallengeInput {
+    userId: String!
+    attackerId: Int!
+    defenderId: Int!
+    description: String! @string(minLength: 20, maxLength: 250)
+    date: String! @date(isAfter: TODAY)
+    pointsInGame: Int! @number(min: 10, max: 200)
   }
 
   type Query {
     login(username: String!, password: String!): Token!
-    users: [User!]!
-    user(id: ID!): User
-    planets: [Planet!]!
-    planet(id: ID!): Planet
-    challenges: [Challenge!]!
-    challenge(id: Int!): Challenge
+    users: [User!]! @auth
+    user(id: ID!): User @auth
+    planets: [Planet!]! @auth
+    planet(id: ID!): Planet @auth
+    challenges: [Challenge!]! @auth
+    challenge(id: Int!): Challenge @auth
   }
 
   type Mutation {
     register(registerInput: RegisterInput): Token!
-    createChallenge(
-      userId: String!
-      attackerId: Int!
-      defenderId: Int!
-      description: String!
-      date: String!
-      pointsInGame: Int!
-    ): Challenge
-    acceptChallenge(userId: String!, challengeId: Int!): Challenge
-    refuseChallenge(userId: String!, challengeId: Int!): Challenge
-    cancelChallenge(userId: String!, challengeId: Int!): Challenge
+    createChallenge(createChallengeInput: CreateChallengeInput): Challenge @auth
+    acceptChallenge(userId: String!, challengeId: Int!): Challenge @auth
+    refuseChallenge(userId: String!, challengeId: Int!): Challenge @auth
+    cancelChallenge(userId: String!, challengeId: Int!): Challenge @auth
     setWinnerChallenge(
       userId: String!
       challengeId: Int!
       winner: String!
-    ): Challenge
+    ): Challenge @auth
   }
 `;
 
